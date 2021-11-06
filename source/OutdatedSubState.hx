5a0bf501feeb42894a7c4ba9452ae8693d33f27c
Cyan <85245679+CyanBoi@users.noreply.github.com>
2021-11-05 20:28:48 -0400

    Initial
    the entire source code

---
+++ source/OutdatedSubState.hx
@@ -0,0 +1,44 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxSubState;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.app.Application;
+
+class OutdatedSubState extends MusicBeatState
+{
+	public static var leftState:Bool = false;
+
+	override function create()
+	{
+		super.create();
+		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+		add(bg);
+		var ver = "v" + Application.current.meta.get('version');
+		var txt:FlxText = new FlxText(0, 0, FlxG.width,
+			"HEY! im to lazy to port this mod to a newer version\nso instead im gonna say that im not good at coding "
+			+ " - Cyan "
+			+ "Press Space to go to mod",
+			32);
+		txt.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
+		txt.screenCenter();
+		add(txt);
+	}
+
+	override function update(elapsed:Float)
+	{
+		if (controls.ACCEPT)
+		{
+			leftState = true;
+			FlxG.switchState(new MainMenuState());
+		}
+		if (controls.BACK)
+		{
+			leftState = true;
+			FlxG.switchState(new MainMenuState());
+		}
+		super.update(elapsed);
+	}
+}
